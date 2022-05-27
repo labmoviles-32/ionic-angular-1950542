d6ca8a3dfe50176f6739d7532d6edd70ccc44615
Maress24 <yulianamares13@gmail.com>
2022-05-27 15:03:11 -0500

    P11

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
   firebase: {
-    apiKey: "...",
+    apiKey: "AIzaSyBBNAzsRn3liQMz39YplEn26dJ-wDNHsBM",
     authDomain: "alumnos-32-3679b.firebaseapp.com",
-    databaseURL: "https://alumnos-32-3679b-default-rtdb.firebaseio.com/alumnos",
+    databaseURL: "https://alumnos-32-3679b-default-rtdb.firebaseio.com",
     projectId: "alumnos-32-3679b",
     storageBucket: "alumnos-32-3679b.appspot.com",
-    messagingSenderId: "..."
+    messagingSenderId: "537421706627",
+    appId: "1:537421706627:web:d1f2fa68787a5f5d4832b6",
+    measurementId: "G-VEJVJ7BDQN"
   },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,27 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "...",
+  firebaseConfig: {
+    apiKey: "AIzaSyBBNAzsRn3liQMz39YplEn26dJ-wDNHsBM",
     authDomain: "alumnos-32-3679b.firebaseapp.com",
-    databaseURL: "https://alumnos-32-3679b-default-rtdb.firebaseio.com/alumnos",
+    databaseURL: "https://alumnos-32-3679b-default-rtdb.firebaseio.com",
     projectId: "alumnos-32-3679b",
     storageBucket: "alumnos-32-3679b.appspot.com",
-    messagingSenderId: "..."
-  }
+    messagingSenderId: "537421706627",
+    appId: "1:537421706627:web:d1f2fa68787a5f5d4832b6",
+    measurementId: "G-VEJVJ7BDQN"
+  },
+  firebase: {
+    apiKey: "AIzaSyBBNAzsRn3liQMz39YplEn26dJ-wDNHsBM",
+    authDomain: "alumnos-32-3679b.firebaseapp.com",
+    databaseURL: "https://alumnos-32-3679b-default-rtdb.firebaseio.com",
+    projectId: "alumnos-32-3679b",
+    storageBucket: "alumnos-32-3679b.appspot.com",
+    messagingSenderId: "537421706627",
+    appId: "1:537421706627:web:d1f2fa68787a5f5d4832b6",
+    measurementId: "G-VEJVJ7BDQN"
+  },
+  production: false
 };
 
 
