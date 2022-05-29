f08e24be1fa8f3f5af20206e80b899875dbeebdc
Maress24 <yulianamares13@gmail.com>
2022-05-28 22:08:41 -0500

    PIA

---
+++ android/app/src/main/java/com/lista/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lista.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lista.app',
+  appName: '2ABRIL',
+  webDir: 'dist/2-abril',
+  bundledWebRuntime: false
+};
+
+export default config;
